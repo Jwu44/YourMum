d36974a6c0ce9efb49c129bd297fda1f6ff47ece
Justin Wu <justinwu@Justins-MacBook-Pro.local>
2024-08-09 12:24:25 +1000

    latest state of implementing blue visual indicator to show where to drag task row

--- backend/services/colab_integration.py
+++ backend/services/colab_integration.py
@@ -4,7 +4,7 @@ import urllib3
 # Disable InsecureRequestWarning
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
-COLAB_BASE_URL = "https://2103-34-82-29-252.ngrok-free.app" 
+COLAB_BASE_URL = "https://56d2-34-134-72-237.ngrok-free.app" 
 
 def process_user_data(user_data):
     colab_url = f"{COLAB_BASE_URL}/process_user_data" # Replace with your actual Google Colab URL
