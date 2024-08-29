57b8d632cea9e127f25b66b50ec7186374abc27f
Justin Wu <justinwu@Justins-MacBook-Pro.local>
2024-08-30 09:42:13 +1000

    added is_subtask section, but section detection is not working

--- backend/models/task.py
+++ backend/models/task.py
@@ -1,9 +1,16 @@
 class Task:
-    def __init__(self, id, text, categories=None):
+    def __init__(self, id, text, categories=None, is_subtask=False):
         self.id = id
         self.text = text
         self.categories = set(categories) if categories else set()
         self.completed = False
+        self.is_subtask = is_subtask
+        self.is_section = False
+        self.section = None
+        self.parent_id = None
+        self.level = 0
+        self.section_index = 0
+        self.type = 'task'
         self.additional_attributes = {}
 
     def add_category(self, category):
@@ -27,14 +34,15 @@ class Task:
             "text": self.text,
             "categories": list(self.categories),
             "completed": self.completed,
+            "is_subtask": self.is_subtask,
             **self.additional_attributes
         }
 
     @classmethod
     def from_dict(cls, data):
-        task = cls(data["id"], data["text"], data.get("categories", []))
+        task = cls(data["id"], data["text"], data.get("categories", []), data.get("is_subtask", False))
         task.completed = data.get("completed", False)
         for key, value in data.items():
-            if key not in ["id", "text", "categories", "completed"]:
+            if key not in ["id", "text", "categories", "completed", "is_subtask"]:
                 task.set_attribute(key, value)
         return task
