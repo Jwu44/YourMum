98ae7ccc9b89d90f661bd1101d7e37d47c45cb00
Justin Wu <justinwu@Justins-MacBook-Pro.local>
2024-10-23 15:29:21 +1100

    reverted to old version, now stuck on saving task edits makes schedule unresponsive

--- backend/services/colab_integration.py
+++ backend/services/colab_integration.py
@@ -6,7 +6,7 @@ import uuid
 # Disable InsecureRequestWarning
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
-COLAB_BASE_URL = "https://84a1-34-145-250-107.ngrok-free.app" 
+COLAB_BASE_URL = "https://c8ec-34-106-219-154.ngrok-free.app" 
 
 def process_user_data(user_data):
     colab_url = f"{COLAB_BASE_URL}/process_user_data"
