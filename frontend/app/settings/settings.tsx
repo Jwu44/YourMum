d4334ff9108bdf06af057a4674bb24c1e6055523
Justin Wu <justin.wu4444@gmail.com>
2025-01-22 17:45:26 +1100

    removed calendar connextion from settings

--- frontend/app/settings/settings.tsx
+++ frontend/app/settings/settings.tsx
@@ -1,12 +1,10 @@
-import { CalendarConnection } from '@/components/parts/CalendarConnection';
-
 export default function SettingsPage() {
   return (
     <div className="container mx-auto p-4">
       <h1 className="text-2xl font-bold mb-6">Settings</h1>
       
       <div className="space-y-6">
-        <CalendarConnection />
+        {/* <CalendarConnection /> */}
         {/* Other settings components */}
       </div>
     </div>
