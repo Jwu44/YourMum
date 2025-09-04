9cbbe6ee9a0d53573d3f3c32500e6b8a6927e22d
Justin Wu <justin.wu4444@gmail.com>
2025-09-04 10:02:22 +1000

    ensuring firebase vars are not hardcoded

--- frontend/auth/firebase.ts
+++ frontend/auth/firebase.ts
@@ -5,13 +5,13 @@ import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCBmlq8Wc5yEuUAkqmKnIoafq_uppShqm8",
-  authDomain: "yourmum-cc74b.firebaseapp.com",
-  projectId: "yourmum-cc74b",
-  storageBucket: "yourmum-cc74b.firebasestorage.app",
-  messagingSenderId: "1044107029932",
-  appId: "1:1044107029932:web:77cc4a6d3a5253b2f79e09",
-  measurementId: "G-MM3H88LNYN"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY || "AIzaSyCBmlq8Wc5yEuUAkqmKnIoafq_uppShqm8",
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN || "yourmum-cc74b.firebaseapp.com",
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || "yourmum-cc74b",
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET || "yourmum-cc74b.firebasestorage.app",
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || "1044107029932",
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID || "1:1044107029932:web:77cc4a6d3a5253b2f79e09",
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID || "G-MM3H88LNYN"
 };
 
 // Initialize Firebase
